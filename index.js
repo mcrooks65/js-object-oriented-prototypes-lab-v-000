a19d4278d9cb011d7e6def0d49e0bdb384443586
Matthew Crooks <mfcrooks@gmail.com>
2018-08-17 20:58:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 
-  this.veto = function(){
+  this.prototype.veto = function(){
     return 'No, I must disagree'
   }
   this.approve = function(){
