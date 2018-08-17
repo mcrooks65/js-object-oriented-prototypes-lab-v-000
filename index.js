cb137fdc07a77a57ace6aaf9ebb55a436a17a945
Matthew Crooks <mfcrooks@gmail.com>
2018-08-17 20:58:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 
-  this.prototype.veto = function(){
-    return 'No, I must disagree'
-  }
   this.approve = function(){
     return 'You can do that!'
   }
@@ -20,4 +17,7 @@ function BoardMember(name, homeState, training){
   this.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
+}
+BoardMember.prototype.veto = function(){
+  return 'No, I must disagree'
 }
